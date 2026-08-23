4bff9ff03024215b21aee56ea484057e2e58b16b
mastewaldegu <mastewaldegu45@gmail.com>
2026-08-23 16:28:21 +0300

    chore: add favicon and font

---
+++ fonts/index.ts
@@ -0,0 +1,10 @@
+import { Urbanist } from "next/font/google";
+
+
+export const urbanist = Urbanist({
+  subsets: ["latin"],
+  weight: ["400"],
+  preload: true,
+  variable: "--font-urbanist",
+  display: "swap",
+});
