e94fc6a3ee5fc9c23dfe12b7f2266548c943ac6d
mastewaldegu <mastewaldegu45@gmail.com>
2026-08-23 16:41:22 +0300

    chore: install project dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { urbanist } from "@/fonts";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
 
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +15,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${urbanist.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", urbanist.variable, "font-sans", geist.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
